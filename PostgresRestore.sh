7027ad75fd3760eb9af60117c53c863506c6df82
pikaqu888 <45960127+pikaqu888@users.noreply.github.com>
2021-09-20 12:15:00 +0200

    Update and rename PostgresCitusRestore.sh to PostgresRestore.sh

--- PostgresRestore.sh
+++ PostgresRestore.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 containerName=$1 restoreTime=$2
 
-sudo docker cp $containerName.Backup.tar.gz $containerName:.
-sudo docker exec $containerName bash -c "tar -xzf $containerName.Backup.tar.gz"
+sudo docker exec $containerName bash -c "mkdir backup"
+sudo docker cp $containerName.Backup.tar.gz $containerName:/backup
+sudo docker exec $containerName bash -c "tar -xzf backup/$containerName.Backup.tar.gz -C backup"
 sudo docker exec $containerName bash -c "chown -R postgres:postgres backup/"
 sudo docker exec $containerName bash -c "rm $containerName.Backup.tar.gz"
 sudo docker exec $containerName bash -c "rm -rf /var/lib/postgresql/data/*"
-sudo docker exec $containerName bash -c "cp -r backup/$restoreTime/* /var/lib/postgresql/data/"
+sudo docker exec $containerName bash -c "cp -r backup/*/* /var/lib/postgresql/data/"
 sudo docker exec $containerName bash -c "chown -R postgres:postgres /var/lib/postgresql/data/"
+sudo docker exec $containerName bash -c "rm -rf backup"
 sleep 60
 sudo docker restart $containerName
 
-echo "Finish the Postgres Citus Restore process!"
+echo "Finish the Postgres Restore process!"

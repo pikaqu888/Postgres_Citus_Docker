87d266c9cba5cd80524f641b607874ef368fde8a
pikaqu888 <45960127+pikaqu888@users.noreply.github.com>
2021-09-20 12:14:14 +0200

    Update PostgresCitusBackup.sh

--- PostgresCitusBackup.sh
+++ PostgresCitusBackup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 containerName=$1
 
-sudo docker exec $containerName bash -c "pg_basebackup -U postgres -Xs -P -D /backup/'`date +\"%Y%m%d%H%M%S\"`'"
-sudo docker exec $containerName bash -c "tar -czf $containerName.Backup.tar.gz backup/*"
+date=$(date +"%Y%m%d%H%M%S")
+sudo docker exec $containerName bash -c "pg_basebackup -U postgres -Xs -P -D /backup/$date"
+sudo docker exec $containerName bash -c "tar -czf $containerName.Backup.tar.gz -C /backup $date"
 sudo docker cp $containerName:$containerName.Backup.tar.gz .
 sudo docker exec $containerName bash -c "rm $containerName.Backup.tar.gz"
 
 #done
-echo "Finish the Postgres Citus backup process!"
+echo "Finish the Postgres backup process!"
